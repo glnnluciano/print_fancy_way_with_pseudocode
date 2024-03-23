3af1a16928cb8d7e10b7c50c1371b91d4fc28f5c
glnnluciano <glnnluciano@gmail.com>
2024-03-23 17:52:51 +0800

    Ask user for its name

---
+++ print_fancy_way_with_pseudocode.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# ask user name to input name
+user_name = input("Please input you name: ")
+# ask user  to input dream job
+# ask user to input birthday
+# ask user to input its university 
+# print name in fancy way
+# print dream job in fancy way
